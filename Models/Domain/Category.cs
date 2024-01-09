d5779327113a2093b176a476b659cc94179ba832
Mahedi Hasan Niloy <139734574+hpl-sd-niloy@users.noreply.github.com>
2024-01-09 15:28:11 +0600

    Add project files.

---
+++ Models/Domain/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace Blog.API.Models.Domain
+{
+    public class Category
+    {
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+        public string UrlHandle { get; set; }
+    }
+}
